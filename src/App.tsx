587de13e48a6dde7e3fc729457b934e53f557194
Ilana Sandler <9151o66926@gmail.com>
2023-01-19 15:06:25 +0200

    data products

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import { Product } from './components/Product';
 import { products } from './data/products';
+import { Iproduct } from './models';
 
 function App() {
+  const [products, setProducts] = useState<Iproduct[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  async function fetchProfects() {
+    try {
+      setError('');
+      setLoading(true);
+      const response = await axios.get<Iproduct[]>('https://fakestoreapi.com/products?limit=5');
+      setProducts(response.data);
+      setLoading(false);
+    } catch (e: unknown) {
+      const error = e as Error;
+      setLoading(false);
+      setError(error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchProfects();
+  }, [])
+
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
+      {loading && <p className="text-center">Loading...</p>}
+      {error && <p className="text-center text-red-600">{error}</p>}
       {products.map(product =>  <Product product={product} key={product.id} />)}
 
       {/* <Product product={products[0]}/>
